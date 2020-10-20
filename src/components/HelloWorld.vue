2b5ef9e0612a645a0bd06ae67da79d48a5719f9b
Kutter Thornton <kutterthornton@Kutters-MacBook-Pro.local>
2020-10-20 13:52:58 -0500

    💻 Add base code for Hypper

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1></h1>
+</template>
+
+<script>
+export default {
+  beforeMount() {
+      this.delay(5000);
+  },
+
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    delay(ms) {
+      const startPoint = new Date().getTime();
+      while (new Date().getTime() - startPoint <= ms) {
+        /* wait */
+      }
+    }
+  }
+}
+
+
+
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
